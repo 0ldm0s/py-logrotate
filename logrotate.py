edfaecda57b7bd28ba4164cd62936e297ff67ef1
0ldm0s <oldmos@gmail.com>
2023-03-25 17:22:28 +0800

    增加重启指令

--- logrotate.py
+++ logrotate.py
@@ -11,13 +11,19 @@ jdata: List[Union[List, Dict]] = json.loads(jlist)
 for _conf_ in jdata:
     err_log: str = _conf_["pm2_env"]["pm_err_log_path"]
     out_log: str = _conf_["pm2_env"]["pm_out_log_path"]
+    pm_id: int = _conf_["pm2_env"]["pm_id"]
+    need_restart: bool = False
     if os.path.isfile(err_log) and os.path.getsize(err_log) >= 10485760:
         print(f"now rotating:[{err_log}]")
         err_log_handler = RotatingFileHandler(
             err_log, maxBytes=10485760, backupCount=10)
         err_log_handler.doRollover()
+        need_restart = True
     if os.path.isfile(out_log) and os.path.getsize(out_log) >= 10485760:
         print(f"now rotating:[{out_log}]")
         out_log_handler = RotatingFileHandler(
             out_log, maxBytes=10485760, backupCount=10)
         out_log_handler.doRollover()
+        need_restart = True
+    if need_restart:
+        os.system(f"pm2 restart {pm_id}")
